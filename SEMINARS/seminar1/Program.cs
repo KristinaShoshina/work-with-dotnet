72b4185d1977d75b74f8783866f560d2dd5ab47a
Кристина <kris-shosh@yandex.ru>
2023-03-04 22:09:26 +0300

    Homework

---
+++ SEMINARS/seminar1/Program.cs
@@ -0,0 +1,85 @@
+﻿// Console.WriteLine("Input number");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Quad of a  -> {a * a }");
+
+// Console.WriteLine("Input first number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if( num2 * num2 == num1)
+
+// {
+//     Console.WriteLine($"First number {num1} is quad of second number {num2}");
+// }
+// else
+// {
+//     Console.WriteLine($"First number {num1} is not quad of second number {num2}");
+// }
+
+// Console.WriteLine("Input N");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int current = n * (-1);
+// while(current <= n)
+// {
+//     Console.Write(current + " ");
+//     current++;
+// }
+
+
+// // Console.WriteLine("Inout three-digit number");
+// // int num = Convert.ToInt32(Console.ReadLine());
+// // if (num >= 100 && num <= 999)
+// // {
+// // int remainder = num % 10;
+// // Console.WriteLine($"Last digit of {num} -> {remainder}");
+// // }
+// // else
+// // {
+// //     Console.WriteLine("You input not three-digit number");
+// }
+
+// Console.WriteLine("Input first number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1>num2)
+
+// {
+//     Console.WriteLine("First number>Second number");
+// }
+// else
+// {
+//     Console.WriteLine("Second number>First number");
+// }
+
+// Console.WriteLine("Input first number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input third number");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+// int max = num1;
+// if(num2>max)
+// max = num2;
+// if(num3>max)
+// max = num3;
+// Console.WriteLine(max);
+
+// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if(num%2==0)
+// Console.WriteLine("yes");
+// else
+// Console.WriteLine("no");
+
+// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int max = 2;
+// while(max<=num)
+// {
+//     Console.Write(max + " ");
+//     max = max+2;
+// }
