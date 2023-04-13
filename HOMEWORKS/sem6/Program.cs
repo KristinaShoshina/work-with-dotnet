499143b4957b773ba38559948a4fe3d2411c36f7
Кристина <kris-shosh@yandex.ru>
2023-04-13 19:36:03 +0300

    FinalHomework

--- HOMEWORKS/sem6/Program.cs
+++ HOMEWORKS/sem6/Program.cs
@@ -101,3 +101,68 @@
 // //                         y =  k1 * x + b1   
 // }
 // Main();
+
+
+
+// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+// string Allnumbers(int N)
+// {
+//     if (N == 1)
+//     {
+//         return "1";
+//     }
+//     return N.ToString() + " ," + Allnumbers(N-1);
+// }
+// void Main()
+// {
+//     System.Console.WriteLine(Allnumbers(5));
+// }
+// Main();
+
+// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов
+//  в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// int Sum(int N, int M)
+// {
+//     if (N == M) return M;
+//     return N + Sum(N + 1, M);
+// }
+// void Main()
+// {
+//     System.Console.WriteLine(Sum(1, 15));
+// }
+// Main();
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+// int ack(int n, int m)
+// {
+//     if (n == 0)
+//         return m + 1;
+//     if (m == 0)
+//         return ack (n - 1, 1);
+//     else
+//         return ack(n - 1, ack (n, m - 1));
+// }
+// void Main()
+// {
+// System.Console.WriteLine("Input n");
+// int n = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Input m");
+// int m = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine(ack(n,m));
+// }
+// Main();
